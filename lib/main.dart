e07fb131c7c3e4b261a2fb73313b0afef1ecdaab
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-08-02 20:14:49 +0200

    Extract home page to separate folder home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clone_instagram_ui/home/page/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,116 +17,7 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 73, 72, 72)),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 15, 15, 15),
-        appBar: AppBar(
-          actions: const [
-            Icon(
-              Icons.heart_broken,
-              color: Colors.white,
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: Icon(
-                Icons.message,
-                color: Colors.white,
-              ),
-            ),
-          ],
-          title: const Text(
-            'Instagram',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: const Color.fromARGB(255, 56, 55, 55),
-        ),
-        body: ListView(
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(
-                top: 10,
-                left: 20,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  UserStoryAvatar(),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: CircleAvatar(
-                      radius: 35,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: CircleAvatar(
-                      radius: 35,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: CircleAvatar(
-                      radius: 35,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Divider(
-              height: 0.2,
-              color: Colors.grey,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class UserStoryAvatar extends StatelessWidget {
-  const UserStoryAvatar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(10),
-              child: CircleAvatar(
-                radius: 35,
-              ),
-            ),
-            Positioned(
-              bottom: 10,
-              left: 55,
-              child: InkWell(
-                borderRadius: BorderRadius.circular(100.0),
-                onTap: () {},
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Icon(
-                    Icons.add_circle,
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-        const Text(
-          'Your story',
-          style: TextStyle(color: Colors.white),
-        ),
-      ],
+      home: const HomePage(),
     );
   }
 }
